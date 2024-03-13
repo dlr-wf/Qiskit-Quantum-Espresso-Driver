d5395c61bba124f44baf6bb0d7f351cb8daf62ed
S-Erik <schultheis.e@freenet.de>
2024-03-13 16:10:13 +0100

    Initial commit

--- demo.py
+++ demo.py
@@ -19,86 +19,104 @@ from qiskit_nature.second_q.operators.symmetric_two_body import S1Integrals
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
 
-    hdf5_file = os.path.join("qe_files", "out_H2", "H2.save", "wfc1.hdf5")
+    hdf5_file = os.path.join("qe_files", "out_H2 spin", "H2.save", "wfc1.hdf5")
     dat_file = os.path.join("qe_files", "out_H2", "H2.save", "wfc1.dat")
-    dat_up_file = os.path.join("qe_files", "out_H2", "H2.save", "wfcup1.dat")
-    dat_dw_file = os.path.join("qe_files", "out_H2", "H2.save", "wfcdw1.dat")
-    xml_file = os.path.join("qe_files", "out_H2", "H2.save", "data-file-schema.xml")
-    # xml_file = os.path.join(
-    #     "qe_files", "out_H2 copy", "H2.save", "data-file-schema.xml"
-    # )
+    dat_up_file = os.path.join("qe_files", "out_H2 spin", "H2.save", "wfcup1.dat")
+    dat_dw_file = os.path.join("qe_files", "out_H2 spin", "H2.save", "wfcdw1.dat")
+    xml_file = os.path.join(
+        "qe_files", "out_H2 spin", "H2.save", "data-file-schema.xml"
+    )
+    xml_file_copy = os.path.join(
+        "qe_files", "out_H2", "H2.save", "data-file-schema.xml"
+    )
 
     wfc_obj = wfc.Wfc.from_file(dat_file, xml_file)
 
     with open(xml_file, "r", encoding="utf-8") as file:
         xml_dict = xmltodict.parse(file.read())
 
-    # driver = qe_driver.QE_Driver(dat_file, xml_file)
-    driver = qe_driver.QE_Driver([dat_up_file, dat_dw_file], xml_file)
+    driver = qe_driver.QE_Driver(dat_file, xml_file_copy)
+    # driver = qe_driver.QE_Driver([dat_up_file, dat_dw_file], xml_file)
 
     problem = driver.run()
 
-    # Calculate matrix elements
-    from qiskit_nature.second_q.formats.qcschema_translator import (
-        _reshape_2,
-        _reshape_4,
-    )
+    # anna
+    # problem = driver.run(ovlp=True)
+    # ovlp_true = problem.second_q_ops()[1]["AngularMomentum"]
+    # problem = driver.run(ovlp=False)
+    # ovlp_false = problem.second_q_ops()[1]["AngularMomentum"]
+    # ovlp_false
 
+    hamiltonian = problem.hamiltonian
+
+    second_q_op = hamiltonian.second_q_op()
+
+    print(hamiltonian.nuclear_repulsion_energy)
+
+    print(problem.molecule)
+    print(problem.reference_energy)
+    print(problem.num_particles)
+    print(problem.num_spatial_orbitals)
+    print(problem.basis)
+
+    print(problem.properties)
+    print(problem.properties.particle_number)
+    print(problem.properties.angular_momentum)
+    print(problem.properties.magnetization)
+    print(problem.properties.electronic_dipole_moment)
+
+    # ------------------ Solve with NumPyMinimumEigensolver ------------------
+    mapper = JordanWignerMapper()
+    algo = NumPyMinimumEigensolver()
+    algo.filter_criterion = problem.get_default_filter_criterion()
+
+    solver = GroundStateEigensolver(mapper, algo)
+    # FIXME: Get numpy ground state solver running
+    result = solver.solve(problem)
+
+    # coeff_a.T @ overlap @ coeff_b IS NOT THE IDENTITY IF overlap is the idenitity, why?
+    (driver.c_ip_up @ driver.c_ip_dw.T)  # IS NOT IDENTITY
+    (driver.c_ip_up.conj() @ driver.c_ip_dw.T)  # IS IDENTITY
+    # -> .conj() missing in qiskit_nature.second_q.formats.qcschema_translator.get_overlap_ab_from_qcschema?
+    # coeff_a.T @ overlap @ coeff_b -> coeff_a.T.conj() @ overlap @ coeff_b
+    # TODO: release code without overlap parameter and create issue regarding .conj() in
+    #       get_overlap_ab_from_qcschema function
+
+    # anna
+
+    # print(f"Total ground state energy = {result.total_energies[0]:.4f}")
+
+    # result = solver.solve(problem)
+    print(result)
+
+    driver.solve_fci(1)
+    print("FCI eigenvalues:", "\n".join(str(x) for x in driver.fci_evs))
+
+    pauli_sum_op = mapper.map(second_q_op)
+    h_mat = pauli_sum_op.to_matrix()
+    if np.allclose(h_mat, h_mat.T.conj()) is False:
+        print("!!!!!!!!!!!!! Hamiltonian is not hermitian !!!!!!!!!!!!!")
+
+    # ------------------ Solve with Numpy eigh function ------------------
+    eig_vals_all, eig_vecs_all = np.linalg.eigh(h_mat)
+    # print(eig_vals_all[:5])
+    print("# \t Eigenvalue \t\t #electrons\t S²\tS_z")
+
+    num_op = ParticleNumber(pauli_sum_op.num_qubits // 2).second_q_ops()
+    num_op = mapper.map(num_op["ParticleNumber"]).to_matrix()
+    for idx, vals in enumerate(eig_vals_all):
+        state = eig_vecs_all[:, idx]  # [:, idx] NOT [idx]
+        num_op_exp_val = np.round(state.T @ num_op @ state, 3).real
+        if num_op_exp_val == 2:
+            print(f"{idx}\t{vals}\t{num_op_exp_val}")
+
+    Statevector(eig_vecs_all[:, 0]).draw("latex")
+
+    anna
+
+    # ------------------------------------------------------------------------
     h_ij_up, h_ij_dw = driver.calc_h_ij()
     eri_up, eri_dw, eri_dw_up, eri_up_dw = driver.calc_eri()
-
-    # eri_up = eri_up.swapaxes(1, 2).swapaxes(1, 3)
-    # eri_dw = eri_dw.swapaxes(1, 2).swapaxes(1, 3)
-    # eri_dw_up = eri_dw_up.swapaxes(1, 2).swapaxes(1, 3)
-    # eri_up_dw = eri_up_dw.swapaxes(1, 2).swapaxes(1, 3)
-
-    from qiskit_nature.second_q.operators.tensor_ordering import find_index_order
-
-    print(f"eri_up order: {find_index_order(eri_up)}")
-    print(f"eri_dw order: {find_index_order(eri_dw)}")
-    print(f"eri_dw_up order: {find_index_order(eri_dw_up)}")
-    print(f"eri_up_dw order: {find_index_order(eri_up_dw)}")
-
-    print(f"eri_up symmetries: {check_symmetry_two_body_matrix(eri_up)}")
-    print(f"eri_dw symmetries: {check_symmetry_two_body_matrix(eri_dw)}")
-    print(f"eri_dw_up symmetries: {check_symmetry_two_body_matrix(eri_dw_up)}")
-    print(f"eri_up_dw symmetries: {check_symmetry_two_body_matrix(eri_up_dw)}")
-
-    # h_ij_up = h_ij_up.ravel().tolist()
-    # h_ij_dw = h_ij_dw.ravel().tolist()
-    # eri_up = eri_up.ravel().tolist()
-    # eri_dw = eri_dw.ravel().tolist()
-    # eri_dw_up = eri_dw_up.ravel().tolist()
-    # eri_up_dw = eri_up_dw.ravel().tolist()
-
-    # n_orb = driver.wfc_up_obj.nbnd
-    # h_ij_up = _reshape_2(h_ij_up, n_orb)
-    # h_ij_dw = _reshape_2(h_ij_dw, n_orb)
-    # eri_up = _reshape_4(eri_up, n_orb)
-    # eri_dw = _reshape_4(eri_dw, n_orb)
-    # eri_dw_up = _reshape_4(eri_dw_up, n_orb)
-    # eri_up_dw = _reshape_4(eri_up_dw, n_orb)
-
-    # h_ij_up = np.array(h_ij_up).reshape(4, 4)
-    # h_ij_dw = np.array(h_ij_dw).reshape(4, 4)
-    # eri_up = np.array(eri_up).reshape(4, 4, 4, 4)
-    # eri_dw = np.array(eri_dw).reshape(4, 4, 4, 4)
-    # eri_dw_up = np.array(eri_dw_up).reshape(4, 4, 4, 4)
-    # eri_up_dw = np.array(eri_up_dw).reshape(4, 4, 4, 4)
-
-    # h_ij_up = np.zeros_like(h_ij_up)
-    # h_ij_up[0, 0] = 3
-    # h_ij_dw = np.zeros_like(h_ij_dw)
-    # h_ij_dw[0, 0] = -3
-    # eri_up = np.zeros_like(eri_up)
-    # eri_up[0, 0, 0, 0] = 2
-    # eri_dw = np.zeros_like(eri_dw)
-    # eri_dw[0, 0, 0, 0] = -2
-    # eri_dw_up = np.zeros_like(eri_dw_up)
-    # eri_dw_up[0, 0, 0, 0] = -1
-    # eri_up_dw = np.zeros_like(eri_up_dw)
-    # eri_up_dw[0, 0, 0, 0] = 1
-
     nucl_repulsion = calc_matrix_elements.nuclear_repulsion_energy(
         driver.wfc_up_obj.atoms, driver.wfc_up_obj.cell_volume
     )
@@ -157,195 +175,84 @@ if __name__ == "__main__":
     qiskit_energy.nuclear_repulsion_energy = nucl_repulsion
     qiskit_problem = ElectronicStructureProblem(qiskit_energy)
 
-    # # number of particles for spin-up, spin-down
-    # qiskit_problem.num_particles = num_particles
-    # qiskit_problem.num_spatial_orbitals = driver.wfc_up_obj.nbnd
+    # Overlap is identity check
 
-    # qiskit_problem.reference_energy = driver.reference_energy
-    # problem = qiskit_problem
-    # problem = driver.to_qiskit_problem_old()
+    evc_up_dw = np.concatenate([driver.c_ip_dw, driver.c_ip_up])
+    # evc_up_dw = driver.c_ip_up
+    # overlap = np.einsum("ij, kj -> ik", evc_up_dw.conj(), evc_up_dw)
+    # # overlap matrix has to be of shape (nao, nao) where nao is the
+    # # number of atomic orbitals which are plane-waves in our case
+    # overlap = np.einsum("ji, jk -> ik", evc_up_dw.conj(), evc_up_dw)
+    overlap = evc_up_dw.T.conj() @ evc_up_dw
+    mo_coeff_a = driver.c_ip_up.T
+    mo_coeff_b = driver.c_ip_dw.T
+    (evc_up_dw @ evc_up_dw.conj().T).shape
+    # plt.imshow((mo_coeff_a.T @ overlap @ mo_coeff_b).real, vmin=-1.0, vmax=1.0)
+    # plt.colorbar()
+    # (mo_coeff_a.T @ overlap @ mo_coeff_b).max()
 
-    hamiltonian = problem.hamiltonian
-    # np.allclose(hamiltonian.electronic_integrals.alpha_beta["++--"], eri_dw_up)
-    # np.allclose(
-    #     hamiltonian.electronic_integrals.beta_alpha["++--"],
-    #     hamiltonian.electronic_integrals.alpha_beta["++--"],
-    # )
-    # hamiltonian.electronic_integrals.equiv(integrals)
-    # hamiltonian.electronic_integrals.alpha.equiv(integrals.alpha)
-    # Not equivalent because _label_template is None for integrals obj
-    # hamiltonian.electronic_integrals.alpha["++--"]._label_template
-    # integrals.alpha["++--"]._label_template
-    # np.allclose(hamiltonian.electronic_integrals.alpha_beta["++--"], integrals.beta_alpha["++--"])
+    # plt.imshow((driver.c_ip_dw.conj() @ evc_up_dw.T.conj() @ evc_up_dw @ driver.c_ip_dw.T).real)
+    ovlp = (
+        driver.c_ip_dw.conj() @ evc_up_dw.T.conj() @ evc_up_dw @ driver.c_ip_dw.T
+    ).real  # is 2 times identity, why?
+    # overlap calculated in qcschema_translator.get_overlap_ab_from_qcschema
+    # does not apply .conj() on the first matrix
+    # Note that coeff_a in qcschema_translator.get_overlap_ab_from_qcschema
+    # has shape (nao, nmo) but driver.c_ip_dw has shape (nmo, nao)
 
-    # problem.hamiltonian.second_q_op()
-    # qiskit_problem.hamiltonian.second_q_op()
+    # driver.wfc_up_obj.get_overlaps()
 
-    second_q_op = hamiltonian.second_q_op()
+    from pyscf import gto, scf, ci, ao2mo
 
-    # for i in range(4):
-    #     for j in range(4):
-    #         ij = hamiltonian.second_q_op()[f"+_{i} -_{j}"]
-    #         ij_plus_4 = hamiltonian.second_q_op()[f"+_{i+4} -_{j+4}"]
-
-    #         assert ij == ij_plus_4, f"i|j: {i}|{j}, ij: {ij}, ij_plus_4: {ij_plus_4}"
-
-    # for i in range(4):
-    #     for j in range(4):
-    #         for k in range(4):
-    #             for l in range(4):
-    #                 ijkl = hamiltonian.second_q_op()[f"+_{i} +_{j} -_{k} -_{l}"]
-    #                 ijkl_plus_4 = hamiltonian.second_q_op()[
-    #                     f"+_{i+4} +_{j+4} -_{k+4} -_{l+4}"
-    #                 ]
-
-    #                 assert (
-    #                     ijkl == ijkl_plus_4
-    #                 ), f"i|j|k|l: {i}|{j}|{k}|{l}, ijkl: {ijkl}, ijkl_plus_4: {ijkl_plus_4}"
-
-    print(hamiltonian.nuclear_repulsion_energy)
-
-    print(problem.molecule)
-    print(problem.reference_energy)
-    print(problem.num_particles)
-    print(problem.num_spatial_orbitals)
-    print(problem.basis)
-
-    print(problem.properties)
-    print(problem.properties.particle_number)
-    print(problem.properties.angular_momentum)
-    print(problem.properties.magnetization)
-    print(problem.properties.electronic_dipole_moment)
-
-    # ------------------ Solve with NumPyMinimumEigensolver ------------------
-    mapper = JordanWignerMapper()
-    algo = NumPyMinimumEigensolver()
-    algo.filter_criterion = problem.get_default_filter_criterion()
-
-    solver = GroundStateEigensolver(mapper, algo)
-    # result = solver.solve(problem)
-
-    # print(f"Total ground state energy = {result.total_energies[0]:.4f}")
-
-    # result = solver.solve(problem)
-    # print(result)
-
-    print("FCI START")
-    driver.solve_fci(10)
-    print("\n".join(str(x) for x in driver.fci_evs))
-    print("FCI END")
-
-    pauli_sum_op = mapper.map(second_q_op)
-    h_mat = pauli_sum_op.to_matrix()
-    if np.allclose(h_mat, h_mat.T.conj()) is False:
-        print("!!!!!!!!!!!!! Hamiltonian is not hermitian !!!!!!!!!!!!!")
-
-    # ------------------ Solve with Numpy eigh function ------------------
-    eig_vals_all, eig_vecs_all = np.linalg.eigh(h_mat)
-    # print(eig_vals_all[:5])
-    print("# \t Eigenvalue \t\t #electrons\t S²\tS_z")
-
-    num_op = ParticleNumber(pauli_sum_op.num_qubits // 2).second_q_ops()
-    num_op = mapper.map(num_op["ParticleNumber"]).to_matrix()
-    for idx, vals in enumerate(eig_vals_all):
-        state = eig_vecs_all[:, idx]  # [:, idx] NOT [idx]
-        num_op_exp_val = np.round(state.T @ num_op @ state, 3).real
-        if num_op_exp_val == 2:
-            print(f"{idx}\t{vals}\t{num_op_exp_val}")
-
-    # Statevector(eig_vecs_all[:, 0]).draw("latex")
-
-    # problem = h_ks.to_qiskit_problem()
-
-    # fci_energy = h_ks.solve_fci()
-
-    # h_ks.solve_vqe()
-    # vqe_energy = h_ks.qiskit_elec_struc_result.total_energies
-
-    # print(f"vqe energy: {vqe_energy}")
-    # print(f"fci energy: {fci_energy}")
-    # print(f"VQE Result:\n{h_ks.qiskit_elec_struc_result}")
-
-    # print("Done!")
-
-    # import pyscf
-    # import pyscf.fci
-
-    # h_ij_up, h_ij_dw = driver.calc_h_ij()
-    # eri_up, eri_dw, eri_dw_up, eri_up_dw = driver.calc_eri()
-
-    # # # Transform ERIs to chemist's index order
-    # eri_up = eri_up.swapaxes(1, 2).swapaxes(1, 3)
-    # eri_dw = eri_dw.swapaxes(1, 2).swapaxes(1, 3)
-    # eri_dw_up = eri_dw_up.swapaxes(1, 2).swapaxes(1, 3)
-    # eri_up_dw = eri_dw_up.swapaxes(1, 2).swapaxes(1, 3)
-
-    # nucl_repulsion = calc_matrix_elements.nuclear_repulsion_energy(
-    #     driver.wfc_up_obj.atoms, driver.wfc_up_obj.cell_volume
-    # )
-
-    # nelec = (int(np.sum(driver.occupations_up)), int(np.sum(driver.occupations_dw)))
-    # print(f"nelec: {nelec}")
-
-    # # FCI calculation
-    # nroots = 1
-
-    # norb = driver.wfc_up_obj.nbnd
-    # fcisolver = pyscf.fci.direct_uhf.FCISolver()
-    # fci_evs, fci_evcs = fcisolver.kernel(
-    #     h1e=(h_ij_up.real, h_ij_dw.real),
-    #     eri=(
-    #         eri_up.real,
-    #         eri_up_dw.swapaxes(0,2).swapaxes(2,3).real,
-    #         eri_dw.real,
-    #     ),  # aa, ab, bb (a=up, b=down)
-    #     norb=norb,
-    #     nelec=nelec,
-    #     nroots=nroots,
-    # )
-    # print(fci_evs)
-
-    # PYSCF alpha-beta two-body test
-    # from pyscf import gto, scf, ci, ao2mo
     # from qiskit_nature.second_q.operators.symmetric_two_body import fold
-    # mol = gto.M(
-    #     atom = 'O 0 0 0; O 0 0 1.2',  # in Angstrom
-    #     basis = 'sto-3g',
-    #     spin = 2
-    # )
-    # mf = scf.HF(mol).run() # this is UHF
-    # # myci = ci.CISD(mf).run() # this is UCISD
-    # # print('UCISD total energy = ', myci.e_tot)
 
-    # mo_coeff, mo_coeff_b = mf.mo_coeff
-    # np.allclose(mo_coeff, mo_coeff_b)
-    # # plt.imshow(mo_coeff-mo_coeff_b, cmap="coolwarm")
-    # # plt.colorbar()
+    # # mol = gto.M(atom="H 0 0 0; H 0 0 1.2", basis="sto-3g", spin=2)  # in Angstrom
+    # # mf = scf.HF(mol).run() # this is UHF
+    # mf.get_ovlp().shape
+    # # plt.imshow(mf.get_ovlp())
+    # # mf.mo_coeff.shape
 
-    # eri_mo_ba = fold(ao2mo.general(
-    #         mol,
-    #         [mo_coeff_b, mo_coeff_b, mo_coeff, mo_coeff]
-    #     ))
-    # eri_mo_bb = fold(ao2mo.full(mol, mo_coeff_b))
-    # eri = mol.intor("int2e")
-    # eri_mo = fold(ao2mo.full(mol, mo_coeff))
-    # eri_mo.shape
-    # # ao2mo.full(mol, mo_coeff_b).shape
-
-    # eri_mo_ba._array.shape
-
-    # # check_symmetry_two_body_matrix(eri_mo)
-
-    # # mo_coeff.shape
-
-    # # PYSCF driver test
-    # from qiskit_nature.units import DistanceUnit
+    # # plt.imshow(mf.mo_coeff[0].T @ mf.get_ovlp() @ mf.mo_coeff[1])
     # from qiskit_nature.second_q.drivers import PySCFDriver
+    # from qiskit_nature.second_q.drivers import MethodType
 
-    # driver = PySCFDriver(
-    #     atom="O 0 0 0; O 0 0 1.2", basis="sto-3g", spin=2  # in Angstrom
+    # x = PySCFDriver(
+    #     atom="H 0 0 0; H 0 0 1.2",  # in Angstrom
+    #     basis="sto-3g",
+    #     spin=2, # mole.spin is the number of unpaired electrons 2S, i.e. the difference between the number of alpha and beta electrons.
+    #     method=MethodType.UHF,
     # )
+    # x.method
+    # x.run()
+    # x.to_problem().second_q_ops()[1]["AngularMomentum"]
+    # # type(x.to_qcschema().wavefunction.scf_orbitals_b)
+    # # x._calc.mo_coeff.shape
+    # plt.imshow(mf.mo_coeff[0].T @ mf.get_ovlp() @ mf.mo_coeff[1])
 
-    # problem = driver.run()
+    # # problem = x.run()
 
-    # problem.hamiltonian.electronic_integrals.alpha["++--"].reshape((10,10,10,10))
+    # # mapper = JordanWignerMapper()
+    # # algo = NumPyMinimumEigensolver()
+    # # algo.filter_criterion = problem.get_default_filter_criterion()
+
+    # # solver = GroundStateEigensolver(mapper, algo)
+    # # result = solver.solve(problem)
+    # # print(result)
+
+    # from qiskit_nature.second_q.formats.qcschema_translator import get_overlap_ab_from_qcschema
+    # get_overlap_ab_from_qcschema(x.to_qcschema())
+
+    from pyscf import gto, scf, ci, ao2mo
+
+    mol = gto.M(atom="H 0 0 0; H 0 0 1.2", basis="sto-3g", spin=0)  # in Angstrom
+    mf = scf.UHF(mol).run()  # this is UHF
+    overlap = mf.get_ovlp()
+    # overlap = mf.mo_coeff[0] @ mf.mo_coeff[0].T
+    mo_coeff_a = mf.mo_coeff[0]
+    mo_coeff_b = mf.mo_coeff[1]
+    # plt.imshow((mo_coeff_a.T @ overlap @ mo_coeff_b).real, vmin=-1.0, vmax=2.0)
+    # plt.imshow((mo_coeff_b.T @ mo_coeff_b).real, vmin=-1.0, vmax=2.0)
+    # plt.colorbar()
+    (mo_coeff_b.T @ mo_coeff_b).real
+    # mf.mo_coeff.shape
+    # mol.nelec
---
+++ qiskit_nature_qe/my_ground_state_eigensolver.py
@@ -0,0 +1,123 @@
+# This code is part of a Qiskit project.
+#
+# (C) Copyright IBM 2020, 2023.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""Ground state computation using a minimum eigensolver."""
+
+from __future__ import annotations
+
+import logging
+
+from qiskit_algorithms import MinimumEigensolver
+from qiskit.quantum_info import SparsePauliOp
+
+from qiskit_nature.second_q.operators import SparseLabelOp
+from qiskit_nature.second_q.mappers import QubitMapper
+from qiskit_nature.second_q.problems import BaseProblem
+from qiskit_nature.second_q.problems import EigenstateResult
+
+from qiskit_nature.second_q.algorithms.ground_state_solvers import GroundStateSolver
+
+LOGGER = logging.getLogger(__name__)
+
+
+class GroundStateEigensolver(GroundStateSolver):
+    """Ground state computation using a minimum eigensolver."""
+
+    def __init__(
+        self,
+        qubit_mapper: QubitMapper,
+        solver: MinimumEigensolver,
+    ) -> None:
+        # pylint: disable=unused-argument
+        """
+        Args:
+            qubit_mapper: The :class:`~qiskit_nature.second_q.mappers.QubitMapper` instance that
+                converts a second quantized operator to qubit operators.
+            solver: Minimum Eigensolver object.
+        """
+        super().__init__(qubit_mapper)
+        self._solver = solver
+
+    @property
+    def solver(self) -> MinimumEigensolver:
+        return self._solver
+
+    def supports_aux_operators(self):
+        return self.solver.supports_aux_operators()
+
+    def solve(
+        self,
+        problem: BaseProblem,
+        aux_operators: dict[str, SparseLabelOp | SparsePauliOp] | None = None,
+    ) -> EigenstateResult:
+        """Compute Ground State properties.
+
+        Args:
+            problem: A class encoding a problem to be solved.
+            aux_operators: Additional auxiliary operators to evaluate.
+
+        Returns:
+            An interpreted :class:`~.EigenstateResult`. For more information see also
+            :meth:`~.BaseProblem.interpret`.
+        """
+        main_operator, aux_ops = self.get_qubit_operators(problem, aux_operators)
+        raw_mes_result = self.solver.compute_minimum_eigenvalue(main_operator, aux_ops)
+
+        return raw_mes_result
+
+        eigenstate_result = EigenstateResult.from_result(raw_mes_result)
+        result = problem.interpret(eigenstate_result)
+        return result
+
+    def get_qubit_operators(
+        self,
+        problem: BaseProblem,
+        aux_operators: dict[str, SparseLabelOp | SparsePauliOp] | None = None,
+    ) -> tuple[SparsePauliOp, dict[str, SparsePauliOp] | None]:
+        # Note that ``aux_ops`` contains not only the transformed ``aux_operators`` passed by the
+        # user but also additional ones from the transformation
+        main_second_q_op, aux_second_q_ops = problem.second_q_ops()
+
+        main_operator = self._qubit_mapper.map(main_second_q_op)
+        aux_ops = self._qubit_mapper.map(aux_second_q_ops)
+
+        if aux_operators is not None:
+            for name_aux, aux_op in aux_operators.items():
+                if isinstance(aux_op, SparseLabelOp):
+                    converted_aux_op = self._qubit_mapper.map(aux_op)
+                else:
+                    converted_aux_op = aux_op
+
+                if name_aux in aux_ops.keys():
+                    LOGGER.warning(
+                        "The key '%s' was already taken by an internally constructed auxiliary "
+                        "operator! The internal operator was overridden by the one provided manually. "
+                        "If this was not the intended behavior, please consider renaming "
+                        "this operator.",
+                        name_aux,
+                    )
+                if converted_aux_op is not None:
+                    # The custom op overrides the default op if the key is already taken.
+                    aux_ops[name_aux] = converted_aux_op
+                else:
+                    LOGGER.warning(
+                        "The manually provided operator '%s' got reduced to `None` in the mapping "
+                        "process. This can occur for example when it does not commute with the "
+                        "hamiltonian after applying the determined symmetry reductions. Thus, this "
+                        "operator will not be used!",
+                        name_aux,
+                    )
+
+        # if the eigensolver does not support auxiliary operators, reset them
+        if not self.solver.supports_aux_operators():
+            aux_ops = None
+        return main_operator, aux_ops
